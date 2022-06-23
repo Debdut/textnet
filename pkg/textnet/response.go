e4023e6e26e8b8c2793de9f4c1afd4ac2a0f973a
Debdut Karmakar <iawaiponly@gmail.com>
2022-06-23 06:13:43 +0530

    textnet: response :update

--- pkg/textnet/response.go
+++ pkg/textnet/response.go
@@ -1,9 +1,12 @@
 package textnet
 
-import st "github.com/debdut/textnet/pkg/state"
+import (
+	"strings"
+
+	st "github.com/debdut/textnet/pkg/state"
+)
 
 func Response(identifier string, message string) string {
-	reply := "unknown command"
 	state, ok := STATES[identifier]
 	if !ok {
 		state = st.State{
@@ -12,8 +15,29 @@ func Response(identifier string, message string) string {
 		}
 	}
 
+	processedMessage := strings.Trim(message, "\n\t .")
+	action := st.IdentifyAction(state, processedMessage)
+	if action.Type == "Unknown" {
+		if action.Payload != nil {
+			reply := action.Payload.(*st.Unknown).Message
+			if reply != "" {
+				return reply
+			}
+		}
+
+		return "unknown action"
+	}
+
+	state, err := st.Machine(state, action)
+	// TODO: explain error
+	if err != nil {
+		return err.Error()
+	}
+
+	// TODO: handle state messages
+
 	// update state
 	STATES[identifier] = state
 
-	return reply
+	return "invalid action in context"
 }
