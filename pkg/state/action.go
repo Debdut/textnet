6318fc5103b7253259036a986a3fc8db8b843cc0
Debdut Karmakar <iawaiponly@gmail.com>
2022-06-23 05:49:03 +0530

    state: identify action

--- pkg/state/action.go
+++ pkg/state/action.go
@@ -1,5 +1,12 @@
 package state
 
+import (
+	"strconv"
+	"strings"
+
+	"github.com/debdut/textnet/pkg/util"
+)
+
 var ActionTypes = []string{
 	"Site: Send",
 	"Site: Next",
@@ -12,6 +19,7 @@ var ActionTypes = []string{
 	"Link: Next",
 	"Link: Prev",
 	"Link: Choose",
+	"Unknown",
 }
 
 type SiteSendPayload struct {
@@ -88,6 +96,12 @@ func (payload *LinkChoosePayload) Type() string {
 	return "LinkChoosePayload"
 }
 
+type Unknown struct{}
+
+func (payload *Unknown) Type() string {
+	return "Unknown"
+}
+
 type Payload interface {
 	Type() string
 }
@@ -95,4 +109,94 @@ type Payload interface {
 type Action struct {
 	Type    string
 	Payload Payload
+}
+
+func IdentifyAction(state State, message string) Action {
+	action := Action{
+		Type: "Unknown",
+	}
+
+	if message == "next" {
+		if state.Type == "Site" {
+			action.Type = "Site: Next"
+			action.Payload = &SiteNextPayload{}
+
+			return action
+		}
+
+		if state.Type == "Link" {
+			action.Type = "Link: Next"
+			action.Payload = &LinkNextPayload{}
+
+			return action
+		}
+	}
+
+	if message == "prev" {
+		if state.Type == "Site" {
+			action.Type = "Site: Prev"
+			action.Payload = &SitePrevPayload{}
+
+			return action
+		}
+
+		if state.Type == "Link" {
+			action.Type = "Link: Prev"
+			action.Payload = &LinkPrevPayload{}
+
+			return action
+		}
+	}
+
+	if message == "link" {
+		if state.Type == "Site" {
+			action.Type = "Link"
+			action.Payload = &LinkPayload{}
+
+			return action
+		}
+	}
+
+	if strings.HasPrefix(message, "search ") {
+		action.Type = "Search: Send"
+		action.Payload = &SearchSendPayload{
+			Query: strings.Replace(message, "search ", "", 1),
+		}
+
+		return action
+	}
+
+	if index, err := strconv.Atoi(message); err == nil {
+		query := uint8(index)
+
+		if state.Type == "Link" {
+			action.Type = "Link: Choose"
+			action.Payload = &LinkChoosePayload{
+				Query: query,
+			}
+
+			return action
+		}
+
+		if state.Type == "Search" {
+			action.Type = "Search: Choose"
+			action.Payload = &SearchChoosePayload{
+				Query: query,
+			}
+
+			return action
+		}
+	}
+
+	if strings.Contains(message, ".") {
+		url, _, err := util.GetURL(message)
+		if err == nil {
+			action.Type = "Site: Send"
+			action.Payload = &SiteSendPayload{
+				Query: url,
+			}
+		}
+	}
+
+	return action
 }
